a46d8c193ac733de5e85af3320abda2f50cd3a0f
Muhammad Rizky <146300355+Gleam-R@users.noreply.github.com>
2026-08-12 08:29:32 +0700

    Delete OOP_TB1_Minggu06n directory
    Rapih-rapih

--- OOP_TB1_Minggu06n/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module OOP_TB1_Minggu06n {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.desktop;
-	
-	opens SisforCDGame to javafx.graphics, javafx.fxml, javafx.base;
-	exports SisforCDGame;
-}
